f443f77beede33b368dd44a8566b5fa926289793
tomgoe88 <tom.goe88@googlemail.com>
2016-08-21 18:26:40 +0200

    k

--- app/src/main/java/com/example/jutom/myapplication/ActivityNeueUebung.java
+++ app/src/main/java/com/example/jutom/myapplication/ActivityNeueUebung.java
@@ -2,7 +2,11 @@ package com.example.jutom.myapplication;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,14 +15,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ActivityNeueUebung extends AppCompatActivity {
     private String listName;
     private EditText uebeschreibung;
     private EditText uename;
     private ImageView ueBild;
     private Button saveButton;
+    String mCurrentPhotoPath;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +41,25 @@ public class ActivityNeueUebung extends AppCompatActivity {
         Log.v("Listname", "Listname = "+listName);
         uebeschreibung= (EditText) findViewById(R.id.uebeschreibung);
         uename=(EditText)findViewById(R.id.uename);
-        ueBild=(ImageView)findViewById(R.id.uebungBild);
+        ueBild=(ImageView)findViewById(R.id.imageView2);
+
         saveButton= (Button)findViewById(R.id.saveButon);
+        ueBild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("Image", "Image is pressed");
+/*                Runnable newRun= new Runnable() {
+                    @Override
+                    public void run() {
+                        dispatchTakePictureIntent();
+                    }
+                };
+                Thread newThread= new Thread(newRun);
+                newThread.start();*/
+                dispatchTakePictureIntent();
+
+            }
+        });
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,16 +80,72 @@ public class ActivityNeueUebung extends AppCompatActivity {
     }
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Log.v("Take Picture", ex.getMessage());
+            }
+            Log.v("Test", photoFile.toString());
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                Log.v("Test", takePictureIntent.getExtras().toString());
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
+    }
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = ueBild.getWidth();
+        int targetH = ueBild.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        ueBild.setImageBitmap(bitmap);
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+/*            Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ueBild.setImageBitmap(imageBitmap);
+            ueBild.setImageBitmap(imageBitmap);*/
+            setPic();
         }
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
     }
 }
